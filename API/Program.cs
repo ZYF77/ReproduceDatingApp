be51ebf04690feceab69a1a48cfd92a7e5d8db7f
Lemon <13275656625@163.com>
2025-12-06 14:38:34 +0800

    引入结构化日志

--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,55 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+//创建Serilog logger
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+    //替换log
+    builder.Host.UseSerilog();
 
-builder.Services.AddControllers();
-builder.Services.AddCors();
+    // Add services to the container.
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer
-    (builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddControllers();
+    builder.Services.AddCors();
+    builder.Services.AddMemoryCache(); // 添加内存缓存服务
 
-var app = builder.Build();
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer
+        (builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configure the HTTP request pipeline.
+    //强类型配置
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    //第一步全局异常处理中间件
+
+    app.UseCors(policy => policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 
-app.UseCors(policy => policy
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "HostAbortedException")
+{
+
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();// 确保所有日志在应用关闭时被写入
+}
 
 
-app.MapControllers();
-
-app.Run();
